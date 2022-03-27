127084d316f4895bebf6340b977b651dee6a46f1
uchaustin <90959696+uchaustin@users.noreply.github.com>
2022-03-27 01:10:15 -0400

    Update github-actions-script.sh

--- github-actions-script.sh
+++ github-actions-script.sh
@@ -27,6 +27,6 @@
           echo Checking for CHANGELOG.md
           if [[ ! -f "CHANGELOG.md" ]]
           then
-              echo "::error::CHANGELOG file not found"
+              echo ::error::CHANGELOG file not found
               exit 1
           fi
