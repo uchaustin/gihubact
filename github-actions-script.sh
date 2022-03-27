5122a8ce0e139719a1f50e2f96babb1815db511c
uchaustin <90959696+uchaustin@users.noreply.github.com>
2022-03-27 13:18:26 -0400

    Update github-actions-script.sh

--- github-actions-script.sh
+++ github-actions-script.sh
@@ -4,7 +4,7 @@
           if [ ! -f "CHANGELOG.md" ]
           then
               echo "::error::CHANGELOG file not found"
-              exit 1
+           
           fi
           
            version_line=$(echo "$( git show :CHANGELOG.md)" | egrep -o '^+# [0-9].*[0-9]*.[0-9]*' | head -1 ) || true
@@ -12,7 +12,7 @@
           then
               echo "::error::CHANGELOG.md must have an updated version number. Git diff didn't show changes"
               echo "::dump::Event name: $GITHUB_EVENT_NAME, and Ref: $GITHUB_REF"
-              exit 1
+            
           fi
         
           
